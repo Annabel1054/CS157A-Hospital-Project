482a913e60612fe3eb079962673d66cb7a98db31
rliao123 <92598518+rliao123@users.noreply.github.com>
2023-05-13 10:15:25 -0700

    Added appointment/prescription form and view all appointments/prescriptions

--- hospital/src/main/java/com/example/hospital/model/Prescription.java
+++ hospital/src/main/java/com/example/hospital/model/Prescription.java
@@ -5,8 +5,6 @@ import lombok.RequiredArgsConstructor;
 
 import javax.persistence.Entity;
 
-import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,25 +12,27 @@ import javax.persistence.Id;
 import javax.persistence.Index;
 import javax.persistence.Table;
 
-@Entity 
-@Table(name = "prescription", indexes=@Index(name = "altIndex", columnList = "prescriptionId", unique = true))
+@Entity
+@Table(name = "prescription", indexes = @Index(name = "altIndex", columnList = "prescriptionId", unique = true))
 @Data
 @RequiredArgsConstructor
 public class Prescription {
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer prescriptionId;
-    
-    @Column(nullable=false)
+
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
     private int quantity;
     @Column(nullable = false)
     private int intake;
     @Column(nullable = false)
     private String frequency;
     @Column(nullable = false)
-    private Date date;
-    @Column(nullable=false)
+    private String date;
+    @Column(nullable = false)
     private int doctorId;
-    @Column(nullable=false)
+    @Column(nullable = false)
     private int patientId;
 }
