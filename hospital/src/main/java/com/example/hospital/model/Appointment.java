6b006c13598fc3c5adf603d9f3129745dcd4d6ee
rliao123 <92598518+rliao123@users.noreply.github.com>
2023-05-13 21:05:25 -0700

    update

--- hospital/src/main/java/com/example/hospital/model/Appointment.java
+++ hospital/src/main/java/com/example/hospital/model/Appointment.java
@@ -19,7 +19,6 @@ public class Appointment {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer appointmentId;
-
     @Column(nullable = false)
     private int doctorId;
     @Column(nullable = false)
